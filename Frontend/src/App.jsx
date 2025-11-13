afa0407ff36cde06a617b57f6b2c6063199ddb6c
Gautam Sharma <155160574+GautamSharma16@users.noreply.github.com>
2025-11-13 20:20:10 +0530

    Refactor App component and update imports

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,22 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './assets/components/landingPage'
-import About from './assets/components/About'
-import Register from './assets/components/Register'
-import Dashboard from './assets/components/Dashboard'
 
-
+import LandingPage from './assets/components/LandingPage.jsx'
+import About from './assets/components/About.jsx'
+import Register from './assets/components/Register.jsx'
+import Dashboard from './assets/components/Dashboard.jsx'
 
 function App() {
-
- 
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LandingPage />}/>
-       <Route path='/about' element={<About />}/>
-       <Route path='/register' element={<Register />}/>
-      <Route path='/dashboard' element={<Dashboard />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
